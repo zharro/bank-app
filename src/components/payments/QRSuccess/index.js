84391f5b824a03a5437e1a5900489910e0ea66b2
Дмитрий Тупалов <d.tupalov@MacBook-Pro-Dmitrij.local>
2018-09-12 14:06:47 +0300

    Add QR workflow

--- src/components/payments/QRSuccess/index.js
+++ src/components/payments/QRSuccess/index.js
@@ -1,20 +1,97 @@
 import React, { Component } from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import {
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListItemText,
+  Button,
+  CardActions,
+  CardMedia,
+} from '@material-ui/core';
 import CheckedCircleIcon from '@material-ui/icons/CheckCircle';
+import qs from 'qs';
+
+const Confirmation = props => (
+  <Grid alignItems={'center'} direction={'column'} container>
+    <Grid item>
+      <Card>
+        <CardMedia
+          style={{ height: 90 }}
+          image={'http://cinet.rzd.ru/dbmm/images/65/121/202'}
+        />
+        <CardContent>
+          <List>
+            <ListItem>
+              <ListItemText primary={'ИНН'} secondary={props.inn} />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={'Расчетный счет'}
+                secondary={props.invoice}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={'Сумма'}
+                secondary={`${props.amount} руб.`}
+              />
+            </ListItem>
+          </List>
+        </CardContent>
+        <CardActions>
+          <Button color={'primary'} onClick={props.onClick}>
+            Подтвердить
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  </Grid>
+);
+
+const Success = () => (
+  <Grid alignItems={'center'} direction={'column'} container>
+    <Grid item>
+      <CheckedCircleIcon style={{ fontSize: 120, color: 'green' }} />
+    </Grid>
+    <Grid item>
+      <Typography variant={'headline'}>Успешный платеж</Typography>
+    </Grid>
+  </Grid>
+);
 
 class QRSuccess extends Component {
-  state = { data: '' };
+  state = { data: {}, hasAgree: false };
+
+  onClick = () => {
+    this.setState({ hasAgree: true });
+  };
+
+  componentDidMount() {
+    const queryParams = qs.parse(this.props.location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    try {
+      this.setState({ data: JSON.parse(queryParams.data) });
+    } catch (error) {}
+  }
 
   render() {
-    return (
-      <Grid alignItems={'center'} direction={'column'} container>
-        <Grid item>
-          <CheckedCircleIcon style={{ fontSize: 120, color: 'green' }} />
-        </Grid>
-        <Grid item>
-          <Typography variant={'headline'}>Успешный платеж</Typography>
-        </Grid>
-      </Grid>
+    const { hasAgree, data } = this.state;
+
+    return !hasAgree ? (
+      <Confirmation
+        inn={data.inn}
+        invoice={data.invoice}
+        amount={data.amount}
+        search={this.props.location.search}
+        onClick={this.onClick}
+      />
+    ) : (
+      <Success />
     );
   }
 }
