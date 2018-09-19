6f5ff01c3d1d6931892f2c3cf0cf738e87797529
Дмитрий Тупалов <d.tupalov@MacBook-Pro-Dmitrij.local>
2018-09-19 17:52:46 +0300

    Добавил полифилл для работы на старых устройствах Андроид

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import '@babel/polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // import * as reducers from './reducers'
-import App from './App'
+import App from './App';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
